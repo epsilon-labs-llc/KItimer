34a9300cf9388ac288bd533c7cec5b3cf313cbd2
tomgly <tomgly.tom1412@gmail.com>
2024-11-30 15:46:58 -0500

    Initial Commit

---
+++ KItimer/KItimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  KItimerApp.swift
+//  KItimer
+//
+//  Created by Tomoya on 11/30/24.
+//
+
+import SwiftUI
+
+@main
+struct KItimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KItimerTests/KItimerTests.swift
@@ -0,0 +1,16 @@
+//
+//  KItimerTests.swift
+//  KItimerTests
+//
+//  Created by Tomoya on 11/30/24.
+//
+
+import Testing
+
+struct KItimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
